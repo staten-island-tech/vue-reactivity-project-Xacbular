a01bd797c931129f44a5b07d9da8cf8ee75d33bf
Xacbular <jacobwu2592@gmail.com>
2026-03-11 13:55:40 -0400

    Fixes

--- vue/src/views/Playlists.vue
+++ vue/src/views/Playlists.vue
@@ -1,28 +1,28 @@
 <template>
-  <main class="playlists-page">
-    <section class="playlists-page__header">
+  <div class="playlists-page">
+    <div class="playlists-page__header">
       <div>
         <h1 class="playlists-page__title">Your Playlists</h1>
         <p class="playlists-page__subtitle">Organize your songs into custom collections.</p>
       </div>
 
       <button class="playlists-page__create-button" @click="createPlaylist">+</button>
-    </section>
+    </div>
 
-    <section v-if="!store.currentUser" class="playlists-page__empty">
+    <div v-if="!store.currentUser" class="playlists-page__empty">
       <h2 class="playlists-page__empty-title">Please log in.</h2>
       <p class="playlists-page__empty-text">
         You need to log in before you can view or create playlists.
       </p>
-    </section>
+    </div>
 
-    <section v-else-if="playlists.length === 0" class="playlists-page__empty">
+    <div v-else-if="playlists.length === 0" class="playlists-page__empty">
       <h2 class="playlists-page__empty-title">No playlists found.</h2>
       <p class="playlists-page__empty-text">Create your first playlist using the + button.</p>
-    </section>
+    </div>
 
-    <section v-else class="playlists-page__grid">
-      <article v-for="playlist in playlists" :key="playlist.id" class="playlists-page__card">
+    <div v-else class="playlists-page__grid">
+      <div v-for="playlist in playlists" :key="playlist.id" class="playlists-page__card">
         <h2 class="playlists-page__card-title">{{ playlist.name }}</h2>
         <p class="playlists-page__card-count">
           {{ playlist.songs.length }} song<span v-if="playlist.songs.length !== 1">s</span>
@@ -35,9 +35,9 @@
         </ul>
 
         <p v-else class="playlists-page__card-empty">This playlist is empty.</p>
-      </article>
-    </section>
-  </main>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
